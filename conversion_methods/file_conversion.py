1563a1a62c4460b828efa2ea2aa025e47bb7ad29
will8889 <57931596+will8889@users.noreply.github.com>
2021-05-31 13:51:53 +0700

    Update file_conversion.py

--- conversion_methods/file_conversion.py
+++ conversion_methods/file_conversion.py
@@ -1,12 +1,15 @@
 import subprocess
 import time
+from os.path import isfile
 
 def convert(input_folder,output_folder,filename):
     ext = filename.split(".")[-1]
     if ext == "pdf": target = "docx"
     else: target = "pdf"
     converted_filename = filename.split(".")[0] + "." + target
-    subprocess.Popen(["abiword","--to=pdf",filename],cwd = input_folder)
-    time.sleep(5)
+    print(ext, target, converted_filename)
+    subprocess.Popen(["abiword","--to="+target,filename],cwd = input_folder)
+    while isfile(input_folder + "/" + converted_filename) == False:
+        continue
     subprocess.Popen(["mv", input_folder + "/" + converted_filename, output_folder])
     return converted_filename
