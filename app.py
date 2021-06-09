74f34d8e6933c077a9c56bbd9ebb29f20b451680
jeremyponto <56902787+jeremyponto@users.noreply.github.com>
2021-06-09 16:09:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from  flask import Flask, redirect, request, render_template, send_from_directory
+from flask import Flask, redirect, request, render_template, send_from_directory
 from werkzeug.utils import secure_filename
 from os.path import join,isfile
 from os import remove,listdir,stat
