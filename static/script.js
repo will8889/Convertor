1b6cd74c2b6a8251567cb271c8076a82bb9ef96e
jeremyponto <56902787+jeremyponto@users.noreply.github.com>
2021-06-09 16:08:11 +0800

    Added comments and minor changes

--- static/script.js
+++ static/script.js
@@ -1,6 +1,9 @@
+/* Define a variable for "menu-toggle input" class which the 3 bars of the recycler view implements. */
 const menuToggle = document.querySelector('.menu-toggle input');
+/* Define a variable for "nav ul" class which the navigation bar implements. */
 const nav = document.querySelector('nav ul');
 
+/* Function to toggle the slide animation which is used to display the recycler view. */
 menuToggle.addEventListener('click', function() {
     nav.classList.toggle('slide');
 })
