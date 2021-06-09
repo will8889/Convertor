010e11ff1287a97b09f3061615a6d4786b254889
will8889 <57931596+will8889@users.noreply.github.com>
2021-06-09 14:46:49 +0700

    added comment in test_app

--- test_app.py
+++ test_app.py
@@ -1,4 +1,6 @@
+#This python file will be used to test if the app.py work or not using pytest
 from app import video_is_supported
 
+#Check if the video_is_supported() from app.py function work properly or not in github action
 def test_video_is_supported():
     assert video_is_supported("test.mp4") == True
